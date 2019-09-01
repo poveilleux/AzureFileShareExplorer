97609dd4a261fa58738962cfd6655bfdac2124bc
Pierre-Olivier Veilleux <poveilleux@genetec.com>
2019-09-01 09:53:22 -0400

    Remove the Api suffix in the project name

--- src/AzureFileShareExplorer/Program.cs
+++ src/AzureFileShareExplorer/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace AzureFileShareExplorer.Api
+namespace AzureFileShareExplorer
 {
     public class Program
     {
--- src/AzureFileShareExplorer/Startup.cs
+++ src/AzureFileShareExplorer/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace AzureFileShareExplorer.Api
+namespace AzureFileShareExplorer
 {
     public class Startup
     {
