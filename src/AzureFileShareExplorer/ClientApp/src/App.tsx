c21220ec86159bcefca7a23a9eaa625c072cba9a
P-O Veilleux <Pierre-Olivier.Veilleux@USherbrooke.ca>
2019-09-06 22:02:23 -0400

    Add font-awesome.

--- src/AzureFileShareExplorer/ClientApp/src/App.tsx
+++ src/AzureFileShareExplorer/ClientApp/src/App.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import './App.scss';
+import { getData } from './mockData';
+import { TreeElement } from './models/treeElement';
+
+const displayElement = (e: TreeElement): JSX.Element => {
+  return (
+    <div>
+      <i className="fas fa-folder" /> {e.name}
+    </div>
+  );
+}
 
 const App: React.FC = () => {
+  const data = getData();
+
   return (
     <div className="container">
       <header className="App-header">
         <h1>Azure File Share Explorer</h1>
       </header>
+      <div>
+        {data.map(displayElement)}
+      </div>
     </div>
   );
 }
