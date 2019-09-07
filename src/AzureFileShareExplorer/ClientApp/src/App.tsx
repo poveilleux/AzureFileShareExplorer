cc8547a083a379298e69effe0dff4dc9c09e751b
P-O Veilleux <Pierre-Olivier.Veilleux@USherbrooke.ca>
2019-09-07 10:38:58 -0400

    Add styling for tree elements (hover, focus, color).

--- src/AzureFileShareExplorer/ClientApp/src/App.tsx
+++ src/AzureFileShareExplorer/ClientApp/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.scss';
 import { getData } from './mockData';
-import { TreeElement } from './models/treeElement';
+import { TreeElement, ElementType } from './models/treeElement';
 
 const displayElement = (e: TreeElement): JSX.Element => {
+  const isFolder = e.type === ElementType.Folder;
+  let dblClick: (() => void) | undefined = undefined;
+  if (isFolder) {
+    dblClick = () => alert("Open folder plz.");
+  }
+
   return (
-    <div>
+    <div key={e.type + e.name} className="tree-element" tabIndex={-1} onDoubleClick={dblClick}>
       <i className="fas fa-folder" /> {e.name}
     </div>
   );
