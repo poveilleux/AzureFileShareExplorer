bd83e1ebfac3f69351d5d5e90ea5f24683808595
P-O Veilleux <Pierre-Olivier.Veilleux@USherbrooke.ca>
2019-09-28 18:37:01 -0400

    UserSecrets + StorageSettings.

---
+++ src/AzureFileShareExplorer/Settings/StorageSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace AzureFileShareExplorer.Settings
+{
+    public class StorageSettings
+    {
+        public const string SectionName = "Storage";
+
+        public string ConnectionString { get; set; }
+    }
+}
