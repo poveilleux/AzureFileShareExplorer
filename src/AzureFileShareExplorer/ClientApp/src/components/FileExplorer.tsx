b6092ebe83ec73fbcd589b4936ed449db31a08b5
P-O Veilleux <Pierre-Olivier.Veilleux@USherbrooke.ca>
2019-10-10 20:39:53 -0400

    Fix NavigationBar and FileExplorer by using React Router. Extract location stuff to helpers.

--- src/AzureFileShareExplorer/ClientApp/src/components/FileExplorer.tsx
+++ src/AzureFileShareExplorer/ClientApp/src/components/FileExplorer.tsx
@@ -1,32 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { getData } from '../mockData';
 import TreeElement from './TreeElement';
+import { appendToLocation } from '../helpers/locationHelpers';
 import { TreeElementModel } from '../models/treeElementModel';
 import NavigationBar from './NavigationBar';
-import { RouteComponentProps } from 'react-router';
 import useReactRouter from 'use-react-router';
 
 const FileExplorer: React.FC = () => {
     const { history, location } = useReactRouter();
 
-    console.log(location.pathname.split('/'));
-    const currentLocation = decodeURIComponent(location.pathname).split('/').filter(x => x);
-    console.log(`currentLocation: ${currentLocation}`);
+    const currentLocation = decodeURIComponent(location.pathname);
 
-    //const [location, setLocation] = useState<string[]>(initialLocation);
     const data = getData(currentLocation);
-
-    // TODO: Read from props and set location to browser.
-    //console.log(props.location.pathname);
 
     function navigate(element: TreeElementModel) {
         if (element.isFolder()) {
-            const newLocation = (location.pathname + "/" + encodeURIComponent(element.name)).replace("//", "/");
-            console.log(location);
-            console.log(newLocation);
+            const newLocation = appendToLocation(location.pathname, element.name);
             history.push(newLocation);
-            // props.history.push("/" + [...location, element.name].join("/"));
-            //setLocation([ ...location, element.name ]);
         }
     }
 
@@ -34,7 +24,7 @@ const FileExplorer: React.FC = () => {
         <div className="container">
             <header className="App-header">
                 <h1>Azure File Share Explorer</h1>
-                <NavigationBar location={location} /*navigateTo={l => setLocation(l)}*/ />
+                <NavigationBar location={currentLocation} navigateTo={l => history.push(l)} />
             </header>
             <div>
                 {data.map((e) => <TreeElement key={e.name} element={e} onDoubleClick={navigate} />)}
