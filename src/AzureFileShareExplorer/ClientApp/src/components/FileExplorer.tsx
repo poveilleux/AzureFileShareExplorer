18bc84cbf5cbdbc9a730cb5dfa05ad2802f46d6b
P-O Veilleux <Pierre-Olivier.Veilleux@USherbrooke.ca>
2019-10-20 20:54:41 -0400

    Make an useAzureFileShare hook to deal with backend calls.

--- src/AzureFileShareExplorer/ClientApp/src/components/FileExplorer.tsx
+++ src/AzureFileShareExplorer/ClientApp/src/components/FileExplorer.tsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import useReactRouter from 'use-react-router';
 import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
-
 import Viewer from 'react-viewer';
 import 'react-viewer/dist/index.css';
 
@@ -10,7 +9,8 @@ import TreeElement from './TreeElement';
 import NavigationBar from './NavigationBar';
 import { downloadFile } from '../helpers/fileHelpers';
 import { appendToLocation } from '../helpers/locationHelpers';
-import { ITreeElementModel, TreeElementModel, FileElementModel } from '../models/treeElementModel';
+import { useAzureFileShare } from '../hooks/useAzureFileShare';
+import { TreeElementModel, FileElementModel } from '../models/treeElementModel';
 import FileViewer from './FileViewer';
 
 function displayErrorMessage(): JSX.Element {
@@ -40,32 +40,12 @@ function getFilePath(currentLocation: string, file: FileElementModel | null, dow
 
 const FileExplorer: React.FC = () => {
     const { history, location } = useReactRouter();
-    const [isLoading, setIsLoading] = useState(false);
-    const [hasError, setHasError] = useState(false);
-    const [data, setData] = useState<TreeElementModel[]>([]);
     const [isImageViewerVisible, setIsImageViewerVisible] = useState(false);
     const [activeIndex, setActiveIndex] = useState(0);
     const [activeFile, setActiveFile] = useState<FileElementModel | null>(null);
 
     const currentLocation = decodeURIComponent(location.pathname);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            setIsLoading(true);
-
-            try {
-                const response = await fetch(`/api${currentLocation}`);
-                const data = await response.json();
-                setData(data.map((d: ITreeElementModel) => TreeElementModel.create(d)));
-            } catch (e) {
-                setHasError(true);
-            } finally {
-                setIsLoading(false);
-            }
-        };
-
-        fetchData();
-    }, [currentLocation]);
+    const [data, isLoading, hasError] = useAzureFileShare(currentLocation);
 
     const images = data
         .filter(x => x.isFile() && (x as FileElementModel).isImage())
---
+++ src/AzureFileShareExplorer/ClientApp/src/hooks/useAzureFileShare.ts
@@ -0,0 +1,34 @@
+﻿import { useEffect, useState } from 'react';
+
+import { ITreeElementModel, TreeElementModel } from '../models/treeElementModel';
+
+/**
+ * Loads the files and folders from an Azure File Share.
+ * 
+ * @param currentLocation The location from which to load the files and folders.
+ */
+export function useAzureFileShare(currentLocation: string): [TreeElementModel[], boolean, boolean] {
+    const [isLoading, setIsLoading] = useState(false);
+    const [hasError, setHasError] = useState(false);
+    const [data, setData] = useState<TreeElementModel[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+
+            try {
+                const response = await fetch(`/api${currentLocation}`);
+                const data = await response.json();
+                setData(data.map((d: ITreeElementModel) => TreeElementModel.create(d)));
+            } catch (e) {
+                setHasError(true);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [currentLocation]);
+
+    return [data, isLoading, hasError];
+}
