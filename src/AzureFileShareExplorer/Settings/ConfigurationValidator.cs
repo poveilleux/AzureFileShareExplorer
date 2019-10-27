49b62e8faf88bce8c115ae7759906fb079622c0e
P-O Veilleux <Pierre-Olivier.Veilleux@USherbrooke.ca>
2019-10-27 16:25:54 -0400

    Validate configurations at Startup.

---
+++ src/AzureFileShareExplorer/Settings/ConfigurationValidator.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace AzureFileShareExplorer.Settings
+{
+    public class ConfigurationValidator : IStartupFilter
+    {
+        private readonly IEnumerable<IValidatableObject> _validatableConfigurations;
+
+        public ConfigurationValidator(IEnumerable<IValidatableObject> validatableConfigurations)
+        {
+            _validatableConfigurations = validatableConfigurations;
+        }
+
+        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+        {
+            var errorMessages = new List<string>();
+
+            foreach (var validatableConfiguration in _validatableConfigurations)
+            {
+                var context = new ValidationContext(validatableConfiguration, serviceProvider: null, items: null);
+                var results = new List<ValidationResult>();
+
+                Validator.TryValidateObject(validatableConfiguration, context, results, true);
+
+                errorMessages.AddRange(results.Select(x => x.ErrorMessage));
+            }
+
+            if (errorMessages.Any())
+            {
+                throw new Exception($"Some configuration are not valid: {string.Join(Environment.NewLine, errorMessages)}");
+            }
+
+            return next;
+        }
+    }
+}
--- src/AzureFileShareExplorer/Settings/StorageSettings.cs
+++ src/AzureFileShareExplorer/Settings/StorageSettings.cs
@@ -1,11 +1,25 @@
-﻿namespace AzureFileShareExplorer.Settings
-{
-    public class StorageSettings
-    {
-        public const string SectionName = "Storage";
+﻿using Microsoft.Azure.Storage;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
+namespace AzureFileShareExplorer.Settings
+{
+    public class StorageSettings : IValidatableObject
+    {
+        public const string Name = "Storage";
+
+        [Required(ErrorMessage = "The " + Name + "." + nameof(ConnectionString) + " is required.")]
         public string ConnectionString { get; set; }
 
+        [Required(ErrorMessage = "The " + Name + "." + nameof(ShareName) + " is required.")]
         public string ShareName { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!CloudStorageAccount.TryParse(ConnectionString, out _))
+            {
+                yield return new ValidationResult($"The {Name}.{nameof(ConnectionString)} has an invalid value.");
+            }
+        }
     }
 }
