764180e4ba3b254603e5d31b6a09fd80807fe540
P-O Veilleux <Pierre-Olivier.Veilleux@USherbrooke.ca>
2019-11-16 22:19:46 -0500

    WIP display name in UI.

--- src/AzureFileShareExplorer/ClientApp/src/components/Layout.tsx
+++ src/AzureFileShareExplorer/ClientApp/src/components/Layout.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import authorizeService from './authorization/AuthorizeService';
 
 const Layout: React.SFC = (props) => {
+    const [name, setName] = useState("");
+
     return (
-        <Container>
-            {props.children}
-        </Container>
+        <div>
+            <Navbar bg="light">
+                <Navbar.Brand>Azure File Share Explorer</Navbar.Brand>
+                <Navbar.Toggle />
+                {
+                    name ?
+                        <Navbar.Collapse className="justify-content-end">
+                            <Nav>
+                                <NavDropdown title="Samichouette" id="user-dropdown">
+                                    <NavDropdown.Item>Sign out</NavDropdown.Item>
+                                </NavDropdown>
+                            </Nav>
+                        </Navbar.Collapse>
+                        : null
+                }
+            </Navbar>
+
+            <Container className="mt-3">
+                {props.children}
+            </Container>
+        </div>
     )
 };
 
