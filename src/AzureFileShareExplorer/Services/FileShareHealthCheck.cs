18281994b1c1bdfd126cd867c3e930716447a394
Pierre-Olivier Veilleux <poveilleux@genetec.com>
2019-12-29 17:28:31 -0500

    Add basic health check

---
+++ src/AzureFileShareExplorer/Services/FileShareHealthCheck.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AzureFileShareExplorer.Settings;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+
+namespace AzureFileShareExplorer.Services
+{
+    public class FileShareHealthCheck : IHealthCheck
+    {
+        private readonly IOptionsMonitor<StorageSettings> _settings;
+
+        private StorageSettings Settings => _settings.CurrentValue;
+
+        public FileShareHealthCheck(IOptionsMonitor<StorageSettings> settings)
+        {
+            _settings = settings;
+        }
+
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(HealthCheckResult.Healthy());
+        }
+    }
+}
