c60f55a7a670c64b4bb3ac35b759cc3060735a99
P-O Veilleux <Pierre-Olivier.Veilleux@USherbrooke.ca>
2020-03-20 19:15:37 -0400

    Fix deployment.

--- Install-AFSE.ps1
+++ Install-AFSE.ps1
@@ -25,12 +25,16 @@ $arguments = @(
     "--set=secretValues.storage.shareName=$ShareName",
     "--set=imagePullSecrets[0].name=dockerhub",
     "--set=service.type=NodePort",
+    "--set=ingress.enabled=true",
+    "--set=ingress.hosts[0].host=afse.com",
+    "--set=ingress.hosts[0].paths[0]=/",
     "--namespace=afse",
     "--debug"
 )
 
-if ($ClientId -or $ClientSecret) {
+if ($ClientId -and $ClientSecret) {
     $arguments += "--set=appsettings.azureAd.enabled=true"
+    $arguments += "--set=appsettings.azureAd.authority=https://login.microsoftonline.com/b8fdc25b-8533-46b2-8ee2-acd69a4cb5df/v2.0"
     $arguments += "--set=secretValues.azureAd.clientId=$ClientId"
     $arguments += "--set=secretValues.azureAd.clientSecret=$ClientSecret"
 }
