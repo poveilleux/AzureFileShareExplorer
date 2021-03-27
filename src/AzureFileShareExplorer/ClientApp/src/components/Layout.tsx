4c611cb077103bdb7a7e56862ef4b2f0b4eef7c5
poveilleux <poveilleux@users.noreply.github.com>
2021-03-26 20:36:14 -0400

    ESLint (#31)
    * ESLint
    
    * Fix build
    
    Co-authored-by: P-O Veilleux <Pierre-Olivier.Veilleux@USherbrooke.ca>

--- src/AzureFileShareExplorer/ClientApp/src/components/Layout.tsx
+++ src/AzureFileShareExplorer/ClientApp/src/components/Layout.tsx
@@ -1,61 +1,61 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Dropdown from 'react-bootstrap/Dropdown';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavLink from 'react-bootstrap/NavLink';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Dropdown from "react-bootstrap/Dropdown";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavLink from "react-bootstrap/NavLink";
 
-import authorizeService from 'src/components/authorization/AuthorizeService';
+import authorizeService from "src/components/authorization/AuthorizeService";
 
-const Layout: React.FC = (props) => {
-    const [name, setName] = React.useState("");
+const Layout: React.FC = (props: React.PropsWithChildren<unknown>) => {
+  const [name, setName] = React.useState("");
 
-    // Subscribe to name updates.
-    React.useEffect(() => {
-        const subscriptionId = authorizeService.subscribe(() => setName(""))
-        return function cleanup() {
-            authorizeService.unsubscribe(subscriptionId);
-        };
-    });
+  // Subscribe to name updates.
+  React.useEffect(() => {
+    const subscriptionId = authorizeService.subscribe(() => setName(""));
+    return function cleanup() {
+      authorizeService.unsubscribe(subscriptionId);
+    };
+  });
 
-    // Load the current user's name.
-    React.useEffect(() => {
-        const fetchName = async () => {
-            const user = await authorizeService.getUser();
-            if (user) {
-                setName(user.name);
-            }
-        };
+  // Load the current user's name.
+  React.useEffect(() => {
+    const fetchName = async () => {
+      const user = await authorizeService.getUser();
+      if (user) {
+        setName(user.name);
+      }
+    };
 
-        fetchName();
-    }, [name]);
+    fetchName();
+  }, [name]);
 
-    return (
-        <div>
-            <Navbar bg="light">
-                <Navbar.Brand href="/">Azure File Share Explorer</Navbar.Brand>
-                <Navbar.Toggle />
-                {
-                    name ?
-                        <Navbar.Collapse className="justify-content-end">
-                            <Nav>
-                                <Dropdown alignRight>
-                                    <Dropdown.Toggle id="user-dropdown" as={NavLink}>{name}</Dropdown.Toggle>
-                                    <Dropdown.Menu>
-                                        <Dropdown.Item onClick={() => authorizeService.signOut()}>Sign out</Dropdown.Item>
-                                    </Dropdown.Menu>
-                                </Dropdown>
-                            </Nav>
-                        </Navbar.Collapse>
-                        : null
-                }
-            </Navbar>
+  return (
+    <div>
+      <Navbar bg="light">
+        <Navbar.Brand href="/">Azure File Share Explorer</Navbar.Brand>
+        <Navbar.Toggle />
+        {
+          name ?
+            <Navbar.Collapse className="justify-content-end">
+              <Nav>
+                <Dropdown alignRight>
+                  <Dropdown.Toggle id="user-dropdown" as={NavLink}>{name}</Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Item onClick={() => authorizeService.signOut()}>Sign out</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Nav>
+            </Navbar.Collapse>
+            : null
+        }
+      </Navbar>
 
-            <Container className="mt-3">
-                {props.children}
-            </Container>
-        </div>
-    )
+      <Container className="mt-3">
+        {props.children}
+      </Container>
+    </div>
+  );
 };
 
 export default Layout;
