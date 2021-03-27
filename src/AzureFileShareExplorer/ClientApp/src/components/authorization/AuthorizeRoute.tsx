5510522913c1486324e86f5f0b74964ab4ae64ba
P-O Veilleux <Pierre-Olivier.Veilleux@USherbrooke.ca>
2021-03-26 20:31:20 -0400

    Fix build

--- src/AzureFileShareExplorer/ClientApp/src/components/authorization/AuthorizeRoute.tsx
+++ src/AzureFileShareExplorer/ClientApp/src/components/authorization/AuthorizeRoute.tsx
@@ -4,61 +4,62 @@ import { RouteProps, Route } from "react-router";
 import authorizeService from "src/components/authorization/AuthorizeService";
 
 interface AuthorizeRouteState {
-    ready: boolean;
-    authenticated: boolean;
+  ready: boolean;
+  authenticated: boolean;
 }
 
 export default class AuthorizeRoute extends React.Component<RouteProps, AuthorizeRouteState> {
-    private _subscription: number | undefined;
+  private _subscription: number | undefined;
 
-    constructor(props: RouteProps) {
-      super(props);
+  constructor(props: RouteProps) {
+    super(props);
 
-      this.state = {
-        ready: false,
-        authenticated: false
-      };
+    this.state = {
+      ready: false,
+      authenticated: false
+    };
+  }
+
+  componentDidMount(): void {
+    this._subscription = authorizeService.subscribe(() => this.authenticationChanged());
+    this.populateAuthenticationState();
+  }
+
+  componentWillUnmount(): void {
+    if (this._subscription) {
+      authorizeService.unsubscribe(this._subscription);
     }
+  }
 
-    componentDidMount(): void {
-      this._subscription = authorizeService.subscribe(() => this.authenticationChanged());
-      this.populateAuthenticationState();
+  render(): React.ReactNode {
+    const { ready, authenticated } = this.state;
+    const redirectUrl = `/user/challenge?returnUrl=${encodeURI(window.location.href)}`;
+
+    if (!ready) {
+      return <div></div>;
+    } else {
+      const { component, ...rest } = this.props;
+      return (
+        <Route {...rest}
+          render={(props): React.ReactElement | undefined => {
+            if (authenticated) {
+              // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+              return React.createElement(component!, props);
+            } else {
+              window.location.href = redirectUrl;
+            }
+          }} />
+      );
     }
+  }
 
-    componentWillUnmount(): void {
-      if (this._subscription) {
-        authorizeService.unsubscribe(this._subscription);
-      }
-    }
+  private async authenticationChanged(): Promise<void> {
+    this.setState({ ready: false, authenticated: false });
+    await this.populateAuthenticationState();
+  }
 
-    render(): React.ReactNode {
-      const { ready, authenticated } = this.state;
-      const redirectUrl = `/user/challenge?returnUrl=${encodeURI(window.location.href)}`;
-
-      if (!ready) {
-        return <div></div>;
-      } else {
-        const { component, ...rest } = this.props;
-        return (
-          <Route {...rest}
-            render={(props): React.ReactElement | undefined => {
-              if (authenticated) {
-                return React.createElement(component, props);
-              } else {
-                window.location.href = redirectUrl;
-              }
-            }} />
-        );
-      }
-    }
-
-    private async authenticationChanged(): Promise<void> {
-      this.setState({ ready: false, authenticated: false });
-      await this.populateAuthenticationState();
-    }
-
-    private async populateAuthenticationState(): Promise<void> {
-      const authenticated = await authorizeService.isAuthenticated();
-      this.setState({ ready: true, authenticated });
-    }
+  private async populateAuthenticationState(): Promise<void> {
+    const authenticated = await authorizeService.isAuthenticated();
+    this.setState({ ready: true, authenticated });
+  }
 }
